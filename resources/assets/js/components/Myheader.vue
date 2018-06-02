1bb6c2e6c3585772c36aa49a472312a0a7f3f6c7
codeincodeworld <emrah@MacBook-Pro-van-Nadine.local>
2018-06-02 14:48:38 +0200

    Updated

---
+++ resources/assets/js/components/Myheader.vue
@@ -0,0 +1,18 @@
+<template>
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="https://bulma.io">
+                <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+                </a>
+                <router-link to="/" class="navbar-item">Home</router-link>
+                <router-link to="/About" class="navbar-item">About</router-link>
+
+                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                </a>
+            </div>
+        </nav>
+</template>
+
